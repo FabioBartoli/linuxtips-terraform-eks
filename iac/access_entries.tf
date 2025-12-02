37b3cfd6d3395aa7b6b1edf442555312a8eb6451
Fabio Bartoli <fabio.bartoli-ext@dex.co>
2025-12-02 12:22:17 -0300

    add user permission

--- iac/access_entries.tf
+++ iac/access_entries.tf
@@ -12,6 +12,14 @@ resource "aws_eks_access_entry" "github_oidc_role" {
   depends_on = [aws_eks_cluster.main]
 }
 
+resource "aws_eks_access_entry" "svc_user" {
+  cluster_name  = aws_eks_cluster.main.id
+  principal_arn = "arn:aws:iam::760337697893:user/svc_github" #SUBSTITUIR PELO USER REAL
+  type          = "STANDARD"
+
+  depends_on = [aws_eks_cluster.main]
+}
+
 resource "aws_eks_access_policy_association" "github_oidc_role_admin" {
   cluster_name  = aws_eks_cluster.main.id
   principal_arn = aws_eks_access_entry.github_oidc_role.principal_arn
@@ -23,4 +31,17 @@ resource "aws_eks_access_policy_association" "github_oidc_role_admin" {
   }
 
   depends_on = [aws_eks_access_entry.github_oidc_role]
+}
+
+resource "aws_eks_access_policy_association" "svc_github_user_admin" {
+  cluster_name  = aws_eks_cluster.main.id
+  principal_arn = aws_eks_access_entry.svc_github_user.principal_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.svc_github_user]
 }
